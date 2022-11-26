193eeda9915e0e03f59f377423d310d919ae7a41
Nick Reuter <nreuter85@gmail.com>
2022-11-26 12:04:37 -0600

    dragonboard: prepare for legacy non-linaro devices
     * Move linaro proprietary blob handling out of common
     * Move partition definitions out of common
    
    Change-Id: I494752db6768c7144339cbd88853305fb6b9065c

--- rb5/device.mk
+++ rb5/device.mk
@@ -45,6 +45,51 @@ PRODUCT_PROPERTY_OVERRIDES += ro.sf.lcd_density=160
 # Copy firmware files
 $(call inherit-product-if-exists, vendor/linaro/rb5/$(EXPECTED_LINARO_VENDOR_VERSION)/device.mk)
 
+# Check vendor package version
+# If you need to make changes to the vendor partition,
+# please modify the source git project here:
+#   https://staging-git.codelinaro.org/linaro/linaro-aosp/aosp-linaro-vendor-package
+include device/linaro/dragonboard/vendor-package-ver.mk
+ifneq (,$(wildcard $(LINARO_VENDOR_PATH)/db845c/$(EXPECTED_LINARO_VENDOR_VERSION)/version.mk))
+  # Unfortunately inherit-product doesn't export build variables from the
+  # called make file to the caller, so we have to include it directly here.
+  include $(LINARO_VENDOR_PATH)/db845c/$(EXPECTED_LINARO_VENDOR_VERSION)/version.mk
+  ifneq ($(TARGET_LINARO_VENDOR_VERSION), $(EXPECTED_LINARO_VENDOR_VERSION))
+    $(warning TARGET_LINARO_VENDOR_VERSION ($(TARGET_LINARO_VENDOR_VERSION)) does not match exiting the build ($(EXPECTED_LINARO_VENDOR_VERSION)).)
+    $(warning Please download new binaries here:)
+    $(warning    $(VND_PKG_URL) )
+    $(warning And extract in the ANDROID_TOP_DIR)
+    # Would be good to error out here, but that causes other issues
+  endif
+else
+  $(warning Missing Linaro Vendor Package!)
+  $(warning Please download new binaries here:)
+  $(warning    $(VND_PKG_URL) )
+  $(warning And extract in the ANDROID_TOP_DIR)
+  # Would be good to error out here, but that causes other issues
+endif
+
+PRODUCT_SOONG_NAMESPACES += \
+    vendor/linaro/rb5/$(EXPECTED_LINARO_VENDOR_VERSION)
+
+# Dynamic partitions
+PRODUCT_BUILD_SUPER_PARTITION := true
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+PRODUCT_USE_DYNAMIC_PARTITION_SIZE := true
+
+# Enable Virtual A/B
+AB_OTA_UPDATER := true
+AB_OTA_PARTITIONS += \
+    product \
+    system \
+    system_ext \
+    vendor
+
+PRODUCT_COPY_FILES += \
+    $(TARGET_KERNEL_DIR)/Image.gz:kernel \
+    device/linaro/dragonboard/fstab.dynamic:$(TARGET_COPY_OUT_RAMDISK)/first_stage_ramdisk/fstab.$(TARGET_HARDWARE) \
+    device/linaro/dragonboard/fstab.dynamic:$(TARGET_COPY_OUT_VENDOR)/etc/fstab.$(TARGET_HARDWARE)
+
 TARGET_DTB := qrb5165-rb5.dtb
 TARGET_HARDWARE := rb5
 TARGET_KERNEL_USE ?= 5.15
--- sm8450/device.mk
+++ sm8450/device.mk
@@ -18,6 +18,48 @@
 $(call inherit-product, frameworks/native/build/tablet-10in-xhdpi-2048-dalvik-heap.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
 
+# Check vendor package version
+# If you need to make changes to the vendor partition,
+# please modify the source git project here:
+#   https://staging-git.codelinaro.org/linaro/linaro-aosp/aosp-linaro-vendor-package
+include device/linaro/dragonboard/vendor-package-ver.mk
+ifneq (,$(wildcard $(LINARO_VENDOR_PATH)/db845c/$(EXPECTED_LINARO_VENDOR_VERSION)/version.mk))
+  # Unfortunately inherit-product doesn't export build variables from the
+  # called make file to the caller, so we have to include it directly here.
+  include $(LINARO_VENDOR_PATH)/db845c/$(EXPECTED_LINARO_VENDOR_VERSION)/version.mk
+  ifneq ($(TARGET_LINARO_VENDOR_VERSION), $(EXPECTED_LINARO_VENDOR_VERSION))
+    $(warning TARGET_LINARO_VENDOR_VERSION ($(TARGET_LINARO_VENDOR_VERSION)) does not match exiting the build ($(EXPECTED_LINARO_VENDOR_VERSION)).)
+    $(warning Please download new binaries here:)
+    $(warning    $(VND_PKG_URL) )
+    $(warning And extract in the ANDROID_TOP_DIR)
+    # Would be good to error out here, but that causes other issues
+  endif
+else
+  $(warning Missing Linaro Vendor Package!)
+  $(warning Please download new binaries here:)
+  $(warning    $(VND_PKG_URL) )
+  $(warning And extract in the ANDROID_TOP_DIR)
+  # Would be good to error out here, but that causes other issues
+endif
+
+# Dynamic partitions
+PRODUCT_BUILD_SUPER_PARTITION := true
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+PRODUCT_USE_DYNAMIC_PARTITION_SIZE := true
+
+# Enable Virtual A/B
+AB_OTA_UPDATER := true
+AB_OTA_PARTITIONS += \
+    product \
+    system \
+    system_ext \
+    vendor
+
+PRODUCT_COPY_FILES += \
+    $(TARGET_KERNEL_DIR)/Image.gz:kernel \
+    device/linaro/dragonboard/fstab.dynamic:$(TARGET_COPY_OUT_RAMDISK)/first_stage_ramdisk/fstab.$(TARGET_HARDWARE) \
+    device/linaro/dragonboard/fstab.dynamic:$(TARGET_COPY_OUT_VENDOR)/etc/fstab.$(TARGET_HARDWARE)
+
 TARGET_HARDWARE := sm8450
 TARGET_KERNEL_USE ?= mainline
 
